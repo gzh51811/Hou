b620bcd3c241c110bd91213dcad1beebbcbd197f
ll <304566206@qq.com>
2019-02-28 17:53:18 +0800

    分类页全选状态改变

--- htgl/public/javascripts/fenlei.js
+++ htgl/public/javascripts/fenlei.js
@@ -8,6 +8,7 @@ $(() => {
     let $close = $('.close');//点击关闭弹窗的按钮
     let $itemList = $(".itemList");//要渲染数据的父元素
 
+    //刚进来的时候 获取数据库信息 渲染页面
     $.ajax({
         type: 'get',
         url: 'http://localhost:3000/fenlei',
@@ -47,14 +48,65 @@ $(() => {
             }
         });
         $man.css('display', 'none');
-    })
+    });
+
+//点击删除按钮
+    //获取选择按钮 用事件委托来写吧
+    // 全选按钮委托父元素
+    $itemList.on('click', '.quanxuan', function () {
+        // 被点击的时候 判断 其他输入框状态 如果没有都选中 就全选 ，如果全选了
+        // 就全不选
+        let xz = document.getElementsByClassName('xz');
+        // console.log(xz);
+        if(this.checked){
+            for(let i = 0;i < xz.length;i++){
+                xz[i].checked = true;
+            }
+        }else{
+            for(let i = 0;i < xz.length;i++){
+                xz[i].checked = false;
+            }
+        }
+    });
+
+    $itemList.on('click','.xz',function(){
+        // 点下面的选择框 根据它们的状态判断全选框的状态
+        let xz = document.getElementsByClassName('xz');
+        let quanxuan = document.getElementsByClassName('quanxuan')[0];
+        let sum = 0;
+        for(let i = 0; i < xz.length;i++){
+            if(xz[i].checked){
+                sum ++;
+            }
+        }
+        if(sum == xz.length){
+            quanxuan.checked = true;
+        }else{
+            quanxuan.checked = false;
+        }
+    });
+
+    $sc.on('click', () => {
+        // 点击之后获取有被选中的input框
+        console.log(666);
+        let sum = 0;
+        for (let i = 0; i < xz.length; i++) {
+            if (xz[i].checked) {
+                sum++;
+                let fenleiList = [];
+                let yeye = xz[i].parentElement.parentElement;
+                fenleiList.push(yeye.dataset.fenlei);
+            }
+        }
+        // 获取到了要删除的分类
+    });
 
 
     // 定义一个渲染函数
     function itemListRendar(arr) {
         let str = `<ul class="clearfix toubu">
                     <li style="width:50px;padding-left:18px;">
-                        <input type="checkbox" class="xz">
+                        <input type="checkbox" class="quanxuan">
                     </li>
                     <li style="width:200px;">
                         分类
@@ -65,7 +117,7 @@ $(() => {
                 </ul>`;
         str += arr.map((item) => {
             return `
-                <ul class="clearfix">
+                <ul class="clearfix" data-fenlei="${item.fenlei}">
                     <li style="width:50px;padding-left:18px;">
                         <input type="checkbox" class="xz">
                     </li>
